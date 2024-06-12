82eea845a38ddd84ab9b78229bb5d112d4b0824b
wnvd <waninaveed998@gmail.com>
2024-06-12 10:56:22 +0530

    fixed animation, add arrows

--- app.js
+++ app.js
@@ -9,20 +9,21 @@ let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
 let timeDom = document.querySelector(".carousel .time");
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 3000;
+let timeRunning = 2000;
 let timeAutoNext = 7000;
 
-nextDom.onclick = function () {
+nextDom.onclick = function() {
   showSlider("next");
 };
 
-prevDom.onclick = function () {
+prevDom.onclick = function() {
   showSlider("prev");
 };
 let runTimeOut;
 let runNextAuto = setTimeout(() => {
   next.click();
 }, timeAutoNext);
+
 function showSlider(type) {
   let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
   let thumbnailItemsDom = document.querySelectorAll(
@@ -38,6 +39,7 @@ function showSlider(type) {
     thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
     carouselDom.classList.add("prev");
   }
+
   clearTimeout(runTimeOut);
   runTimeOut = setTimeout(() => {
     carouselDom.classList.remove("next");
